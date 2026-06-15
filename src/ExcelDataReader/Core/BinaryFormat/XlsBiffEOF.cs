2de444b77d6007934b6397938b8a1a550434c21a
areacookmuse <sedefalyanak57351@gmail.com>
2026-06-15 05:27:24 -0500

    Create src/ExcelDataReader/Core/BinaryFormat/XlsBiffEOF.cs

---
+++ src/ExcelDataReader/Core/BinaryFormat/XlsBiffEOF.cs
@@ -0,0 +1,12 @@
+namespace ExcelDataReader.Core.BinaryFormat;
+
+/// <summary>
+/// Represents BIFF EOF resord.
+/// </summary>
+internal sealed class XlsBiffEof : XlsBiffRecord
+{
+    internal XlsBiffEof(byte[] bytes)
+        : base(bytes)
+    {
+    }
+}
