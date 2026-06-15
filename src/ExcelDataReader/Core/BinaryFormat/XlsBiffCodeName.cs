98a93491bb1b9a842e0d00efc0b17695532223e3
areacookmuse <sedefalyanak57351@gmail.com>
2026-06-15 05:27:09 -0500

    Create src/ExcelDataReader/Core/BinaryFormat/XlsBiffCodeName.cs

---
+++ src/ExcelDataReader/Core/BinaryFormat/XlsBiffCodeName.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace ExcelDataReader.Core.BinaryFormat;
+
+internal sealed class XlsBiffCodeName : XlsBiffRecord
+{
+    private readonly XlsUnicodeString _xlsString;
+
+    internal XlsBiffCodeName(byte[] bytes)
+        : base(bytes)
+    {
+        // BIFF8 only
+        _xlsString = new XlsUnicodeString(bytes, ContentOffset);
+    }
+
+    public string GetValue(Encoding encoding)
+    {
+        return _xlsString.GetValue(encoding);
+    }
+}
